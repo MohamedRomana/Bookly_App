694b641ff6f566cbfdd0d18e9c997a5c4d320a2e
MohamedRomana <MohamedRomana124@gmail.com>
2024-10-12 11:45:47 +0300

    refactor navigation in our app

--- lib/Features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:booklyapp/Features/home/presentation/views/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/view/widgets/sliding_text.dart';
-import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/core/util/app_router.dart';
 import 'package:booklyapp/core/util/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -63,8 +62,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
     Future.delayed(
       const Duration(seconds: 2),
       () {
-        Get.to(() => const HomeView(),
-            transition: Transition.fade, duration: kTransitionDuration);
+        // Get.to(() => const HomeView(),
+        //     transition: Transition.fade, duration: kTransitionDuration);
+        GoRouter.of(context).push(AppRouter.kHomeView);
       },
     );
   }
---
+++ lib/core/util/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:booklyapp/Features/home/presentation/views/home_view.dart';
+import 'package:booklyapp/Features/splash/presentation/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static final router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const SplashView()),
+      GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+
+    ],
+  );
+}

9cc8d07a93da44e6430aa35df246cc27c291f694
MohamedRomana <MohamedRomana124@gmail.com>
2024-10-18 20:18:49 +0300

    fetch similar books

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,22 +1,39 @@
+import 'package:booklyapp/Features/home/presentation/manager/similar_books/similar_books_cubit.dart';
 import 'package:booklyapp/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:booklyapp/core/widgets/custom_circular_indecator_widget.dart';
+import 'package:booklyapp/core/widgets/custom_failure_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListViw extends StatelessWidget {
   const SimilarBooksListViw({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomBookImage(imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbooks.goalkicker.com%2F&psig=AOvVaw22hZu3lUD9BMElquCzdmjU&ust=1729343588247000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjO9IKBmIkDFQAAAAAdAAAAABAE',),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSucces) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomBookImage(
+                    imageUrl:
+                        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbooks.goalkicker.com%2F&psig=AOvVaw22hZu3lUD9BMElquCzdmjU&ust=1729343588247000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjO9IKBmIkDFQAAAAAdAAAAABAE',
+                  ),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomFailureWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomCircularIndecatorWidget();
+        }
+      },
     );
   }
 }
--- lib/core/util/app_router.dart
+++ lib/core/util/app_router.dart
@@ -1,7 +1,12 @@
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo_impl.dart';
+import 'package:booklyapp/Features/home/presentation/manager/similar_books/similar_books_cubit.dart';
 import 'package:booklyapp/Features/home/presentation/views/book_details_view.dart';
 import 'package:booklyapp/Features/home/presentation/views/home_view.dart';
 import 'package:booklyapp/Features/search/presentation/views/search_view.dart';
 import 'package:booklyapp/Features/splash/presentation/view/splash_view.dart';
+import 'package:booklyapp/core/util/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -21,7 +26,10 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+          child:  BookDetailsView(bookModel: state.extra as BookModel,),
+        ),
       ),
       GoRoute(
         path: kSearchView,
